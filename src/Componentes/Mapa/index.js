307f1401f5eec04da4d9ed2027fb642c24c0be71
Drakemaner <79467044+Drakemaner@users.noreply.github.com>
2022-08-23 23:49:48 -0300

    alinhamento mapa

--- src/Componentes/Mapa/index.js
+++ src/Componentes/Mapa/index.js
@@ -4,8 +4,8 @@ import './Mapa.css'
 
 const Mapa = () =>{
     return(
-        <div>
-            <img src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/geojson(%7B%22type%22%3A%22FeatureCollection%22%2C%22features%22%3A%5B%7B%22type%22%3A%22Feature%22%2C%22properties%22%3A%7B%22marker-color%22%3A%22%23462eff%22%2C%22marker-size%22%3A%22medium%22%2C%22marker-symbol%22%3A%22bus%22%7D%2C%22geometry%22%3A%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B-122.25993633270264,37.80988566878777%5D%7D%7D%2C%7B%22type%22%3A%22Feature%22%2C%22properties%22%3A%7B%22marker-color%22%3A%22%23e99401%22%2C%22marker-size%22%3A%22medium%22%2C%22marker-symbol%22%3A%22park%22%7D%2C%22geometry%22%3A%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B-122.25916385650635,37.80629162635318%5D%7D%7D%2C%7B%22type%22%3A%22Feature%22%2C%22properties%22%3A%7B%22marker-color%22%3A%22%23d505ff%22%2C%22marker-size%22%3A%22medium%22%2C%22marker-symbol%22%3A%22music%22%7D%2C%22geometry%22%3A%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B-122.25650310516359,37.8063933469406%5D%7D%7D%5D%7D)/-40.2922,-20.3305,13/1000x600?access_token=pk.eyJ1IjoiZHJha2VtYW5lciIsImEiOiJja3A2dGkyd28wMWIyMnhxczB5MGZjbGw5In0.Chrn6yPnu48OUd80Kpnx6A" />
+        <div >
+            <img className='mapa' src='https://github.com/Drakemaner/Site-Enzo/blob/main/public/fotos/mapa.png?raw=true' alt ='mapa'/>
         </div>
     )
 }
